97fd92c27a862ea3b024d24fb6cbf0d510008cee
Kaisei Yoneyama <97094702+mikrogeophagus@users.noreply.github.com>
2024-03-29 21:42:00 +0900

    Test runner を使用する

--- test.js
+++ test.js
@@ -1,21 +1,25 @@
 'use strict';
 const pug = require('pug');
+const test = require('node:test');
 const assert = require('node:assert');
 
-// pug のテンプレートにおける XSS 脆弱性のテスト
-const html = pug.renderFile('./views/posts.pug', {
-  posts: [
+test('チャットメッセージに含まれる HTML タグがエスケープされる', () => {
+  const html = pug.renderFile(
+    './views/posts.pug',
     {
-      id: 1,
-      content: "<script>alert('test');</script>",
-      postedBy: 'guest1',
-      createdAt: new Date(),
-      updatedAt: new Date()
-    }
-  ],
-  user: 'guest1'
-});
+      posts: [
+        {
+          id: 1,
+          content: '<script>alert("XSS!");</script>',
+          postedBy: 'test_user',
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+      ],
+      user: 'test_user',
+    },
+  );
 
-// スクリプトタグがエスケープされて含まれていることをチェック
-assert(html.includes("&lt;script&gt;alert('test');&lt;/script&gt;"));
-console.log('テストが正常に完了しました');
+  // メッセージの <script> タグがエスケープされていることをチェック
+  assert(html.includes('&lt;script&gt;alert(&quot;XSS!&quot;);&lt;/script&gt;'));
+});
